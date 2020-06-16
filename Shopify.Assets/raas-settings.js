d2b3719c96041364aea26fd2e25e35a8fecd5a18
Sudhanshu Agarwal <sudhanshu.agarwal@loginradius.com>
2020-06-16 19:28:55 +0530

    SSO Bugfixing

--- Shopify.Assets/raas-settings.js
+++ Shopify.Assets/raas-settings.js
@@ -1,46 +1,41 @@
-
-LRObject.$hooks.register('startProcess',function ( ) {
-   jQuery('#fade').show();
-  }
+LRObject.$hooks.register('startProcess', function () {
+  jQuery('#fade').show();
+  $("#loading-spinner").show();
+}
 );
 
 
-LRObject.$hooks.register('endProcess',function ( ) {
+LRObject.$hooks.register('endProcess', function () {
   jQuery('#fade').hide();
-  }
+  $("#loading-spinner").hide();
+}
 );
 
-  function setErrorMessage(msg) {
-    jQuery("#lr-raas-message").show().addClass("loginradius-raas-error-message").text(msg);
-    visibleLoadingSpinner(false);
+function setErrorMessage(msg) {
+  jQuery("#lr-raas-message").show().addClass("loginradius-raas-error-message").text(msg);
+  visibleLoadingSpinner(false);
 
-  }
+}
 
-  function setMessage(msg) {
-    jQuery("#lr-raas-message").show().addClass("loginradius-raas-success-message").text(msg);
-    visibleLoadingSpinner(false);
-  }
-
-
-LRObject.$hooks.register('startProcess',function () {
-  jQuery('.lr_fade').show();
-  }
-);
-
+function setMessage(msg) {
+  jQuery("#lr-raas-message").show().addClass("loginradius-raas-success-message").text(msg);
+  visibleLoadingSpinner(false);
+}
 
 jQuery(window).load(function () {
-  jQuery('.lr_fade').hide();
+  jQuery('.lr_fade').hide();   
 });
 
 function backtoLogin() {
-  LRObject.util.jsonpCall('//' + raasoption.appName + '.hub.loginradius.com/ssologin/logout', function (data) {
-    if (data.ok) {
-      window.lr_raas_settings.sociallogin = 'false';
-      $("#lr-social-traditional-login").show();
-      $("#interfacecontainerdiv").show();
-      $("#no-email-container").hide();
-    }
-  });
+  var logout_options = {};
+  logout_options.onSuccess = function (response) {
+    // On Success
+    //Write your custom code here
+    window.lr_raas_settings.sociallogin = 'false';
+    $("#lr-social-traditional-login").show();
+    $("#interfacecontainerdiv").show();
+    $("#no-email-container").hide();
+  };
 }
 
 jQuery(".site_login_btn").click(function () {
@@ -75,8 +70,8 @@ LRObject.util.ready(function () {
   if (window.lr_raas_settings.registration) {
 
     registration_options.container = window.lr_raas_settings.registration_containerid;
-    LRObject.util.ready(function() {
-    LRObject.init("registration",registration_options);
+    LRObject.util.ready(function () {
+      LRObject.init("registration", registration_options);
     })
 
   }
@@ -85,14 +80,14 @@ LRObject.util.ready(function () {
 
 
 
-      login_options.container = window.lr_raas_settings.login_containerid;
-      LRObject.util.ready(function() {
-        LRObject.init("login",login_options);
-      });
+    login_options.container = window.lr_raas_settings.login_containerid;
+    LRObject.util.ready(function () {
+      LRObject.init("login", login_options);
+    });
 
 
 
-}
+  }
 
   if (window.lr_raas_settings.forgotpassword) {
 
@@ -132,30 +127,30 @@ LRObject.util.ready(function () {
     raasoption.hashTemplate = true;
 
 
-  var custom_interface_option = {};
-  custom_interface_option.templateName = 'loginradiuscustom_tmpl';
-  LRObject.util.ready(function() {
-  LRObject.customInterface(window.lr_raas_settings.sociallogin_interfaceid, custom_interface_option);
+    var custom_interface_option = {};
+    custom_interface_option.templateName = 'loginradiuscustom_tmpl';
+    LRObject.util.ready(function () {
+      LRObject.customInterface(window.lr_raas_settings.sociallogin_interfaceid, custom_interface_option);
+    });
+
+
+    sl_options.container = window.lr_raas_settings.sociallogin_containerid;
+    LRObject.util.ready(function () {
+      LRObject.init('socialLogin', sl_options);
+    });
+
+  }
+
+  LRObject.$hooks.register('socialLoginFormRender', function () {
+    //on social login form render
+    $("#lr-social-traditional-login").hide();
+    $("#interfacecontainerdiv").hide();
+    $("#no-email-container").show();
+    $("#no-email-container").prepend("<h4> We couldn't find your email address, please fill in the form below </h4>");
+    $("#no-email-container").append('    <a onclick="javascript: backtoLogin();" class="backto-login-btn" >Go back to login</a>');
+
+
   });
-
-
-  sl_options.container = window.lr_raas_settings.sociallogin_containerid;
-  LRObject.util.ready(function() {
-    LRObject.init('socialLogin', sl_options);
-  });
-
-}
-
-  LRObject.$hooks.register('socialLoginFormRender',function(){
-   //on social login form render
-   $("#lr-social-traditional-login").hide();
-   $("#interfacecontainerdiv").hide();
-   $("#no-email-container").show();
-   $("#no-email-container").prepend("<h4> We couldn't find your email address, please fill in the form below </h4>");
-   $("#no-email-container").append('    <a onclick="javascript: backtoLogin();" class="backto-login-btn" >Go back to login</a>');
-
-
-});
 
   var maxYear = new Date().getFullYear();
   var minYear = maxYear - 100;
@@ -185,25 +180,7 @@ LRObject.util.ready(function () {
 function hideMessage() {
   jQuery("#lr-raas-message").hide();
 }
-function addJs(url, context) {
-  context = context || document;
-  var head = context.getElementsByTagName('head')[0];
-  var js = context.createElement('script');
-  js.src = url;
-  js.type = "text/javascript";
-  head.appendChild(js);
-  return js;
-}
-function jsonpCall(url, handle) {
-  var func = 'Loginradius' + Math.floor((Math.random() * 1000000000000000000) + 1);
-  window[func] = function (data) {
-    handle(data);
-    window[func] = function () { };
-    document.head.removeChild(js);
-  }
-  var endurl = url.indexOf('?') != -1 ? url + '&callback=' + func : url + '?callback=' + func;
-  var js = addJs(endurl);
-}
+
 function getParameterByName(name) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -211,171 +188,210 @@ function getParameterByName(name) {
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function exchangeMultipassToken(lrtoken, handle) {
-
-  var url = "https://cloud-api.loginradius.com/sso/shopify/api/token?apikey=" + raasoption.apiKey + "&store=" + lrshopifystore + "&access_token=" + lrtoken
+function exchangeMultipassToken(token) {
+  var apiUrl = "https://cloud-api.loginradius.com/sso/shopify/api/token?store=" + lrshopifystore
   if (getParameterByName("checkout_url")) {
-    url = url + "&return_url=" + getParameterByName("checkout_url");
+    apiUrl = apiUrl + "&return_url=" + getParameterByName("checkout_url");
   }
 
-  LRObject.util.jsonpCall(url, handle);
+  $.ajax({
+    url: apiUrl,
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'X-LoginRadius-ApiKey': raasoption.apiKey
+    },
+    success: function (response) {
+      console.log(response);
+      window.location = response.url;
+    }
+  });
 }
 
 
-
 // If found an active session is found, goto the callback function
-var ssologin_options= {};
-
-ssologin_options.onSuccess = function(response) {
-// On Success
-//Write your custom code here
-console.log(response);
-
+var ssologin_options = {};
+ssologin_options.onSuccess = function (response) {
+  // On Success
+  //Write your custom code here
+  console.log(response);
 };
 
 LRObject.util.ready(function () {
-
   LRObject.init("ssoLogin", ssologin_options);
-
 });
 
 
 //Login
 var login_options = {};
+login_options.onSuccess = function (response) {
+  //On Success
 
-login_options.onSuccess = function(response) {
-//On Success
-console.log(response);
-exchangeMultipassToken(response.access_token, function(data){
-  window.location = data.url;
-});
-
+    exchangeMultipassToken(response.access_token);
+ 
 }
-login_options.onError = function(errors) {
-//On Errors
-
-setErrorMessage(errors[0].Message);
-console.log(errors);
+login_options.onError = function (errors) {
+  //On Errors
+    setErrorMessage(errors[0].Message);
+  console.log(errors);
 };
 
-
-
 // Logout Action
-var logout_options= {};
-
-logout_options.onSuccess = function(response) {
-// On Success
-//Write your custom code here
-console.log("Session not found");
-window.location='/account/logout';
-
-
+var logout_options = {};
+logout_options.onSuccess = function (response) {
+  // On Success
+  //Write your custom code here
+  console.log("Session not found");
+  window.location = '/account/logout';
 };
 
 
 var forgotpassword_options = {};
 
-forgotpassword_options.onSuccess = function(response) {
-// On Success
-console.log(response);
-document.getElementsByName("loginradius-forgotpassword")[0].reset();
-setMessage("Password reset information sent to your provided email id, check email for further instructions");
+forgotpassword_options.onSuccess = function (response) {
+  // On Success
+   
+   if(response.IsPosted == true && typeof response.Data !== 'undefined' && response.Data!==null)
+            {
+                setMessage("OTP has been sent to your phone number.");                  
+            }else if(LRObject.options.otpEmailVerification==true && typeof response.Data==='undefined')
+            {
+                setMessage("OTP has been sent to your email address.");              
+            } else if (response.IsPosted == true && typeof (response.Data) === "object") {				
+                if(jQuery('form[name="loginradius-resetpassword"]').length > 0) {
+                  setMessage("Password has been reset successfully.");  
+                    $("#lr-raas-login").show();
+                    $("#login-container").show();
+                    $("#lr-social-traditional-login").show();
+                    $("#lr-raas-forgotpassword").hide();
+                }           
+            }else if (response.IsPosted == true && typeof (response.Data) === "undefined") {
+                if(jQuery('form[name="loginradius-resetpassword"]').length > 0) {                    
+                  setMessage("Password has been reset successfully.");  
+                   $("#lr-raas-login").show();
+                    $("#login-container").show();
+                    $("#lr-social-traditional-login").show();
+                    $("#lr-raas-forgotpassword").hide();
+                } else {
+                  setMessage("Password reset information sent to your provided email id, check email for further instructions");
+                   
+                }
+            }
+ 
 };
-forgotpassword_options.onError = function(errors) {
-// On Errors
-console.log(errors);
-setErrorMessage(errors[0].Message);
+forgotpassword_options.onError = function (errors) {
+  // On Errors
+  
+        if (errors[0].Description != null) {
+                setErrorMessage(errors[0].Description);
+            } else if (errors[0] != null) {
+                setErrorMessage(errors[0].Message);
+            }
 }
 
 
 var verifyemail_options = {};
-verifyemail_options.onSuccess = function(response) {
-// On Success
-$("#lr-social-traditional-login").hide();
-$("#interfacecontainerdiv").hide();
-$("#no-email-container").show();
-$("#no-email-container h4").hide();
+verifyemail_options.onSuccess = function (response) {
+  // On Success
+  $("#lr-social-traditional-login").hide();
+  $("#interfacecontainerdiv").hide();
+  $("#no-email-container").show();
+  $("#no-email-container h4").hide();
 
-
-
-console.log(response);
-setMessage("Email verified, now you can login");
+  console.log(response);
+  setMessage("Email verified, now you can login");
 };
-verifyemail_options.onError = function(errors) {
-// On Errors
-$("#lr-social-traditional-login").hide();
-$("#interfacecontainerdiv").hide();
-$("#redirectologin").show();
-console.log(errors);
-setErrorMessage(errors[0].Message);
+verifyemail_options.onError = function (errors) {
+  // On Errors
+  $("#lr-social-traditional-login").hide();
+  $("#interfacecontainerdiv").hide();
+  $("#redirectologin").show();
+  console.log(errors);
+  setErrorMessage(errors[0].Message);
 }
 
 var resetpassword_options = {};
 
-resetpassword_options.onSuccess = function(response) {
-// On Success
+resetpassword_options.onSuccess = function (response) {
+  // On Success
 
-document.getElementsByName("loginradius-resetpassword")[0].reset();
-console.log(response);
-setMessage("Password reset successfully, now you can login");
-$("#lr-raas-login").show();
-$("#login-container").show();
-$("#lr-social-traditional-login").show();
-$("#resetpassword-container").hide();
+  document.getElementsByName("loginradius-resetpassword")[0].reset();
+  console.log(response);
+  setMessage("Password reset successfully, now you can login");
+  $("#lr-raas-login").show();
+  $("#login-container").show();
+  $("#lr-social-traditional-login").show();
+  $("#resetpassword-container").hide();
 };
-resetpassword_options.onError = function(errors) {
-// On Errors
-console.log(errors);
-setErrorMessage(errors[0].Message);
+resetpassword_options.onError = function (errors) {
+  // On Errors
+  console.log(errors);
+    if (errors[0].Description != null) {
+        setErrorMessage(errors[0].Description);
+    } else if (errors[0] != null) {
+        setErrorMessage(errors[0].Message);
+    }
 }
 
 
-
-
-
 var registration_options = {}
-registration_options.onSuccess = function(response) {
-//On Success
-console.log(response);
-document.getElementsByName("loginradius-registration")[0].reset();
-
-setMessage("Email for verification has been sent to your provided email, please check email for further instructions");
+registration_options.onSuccess = function (response) {
+  //On Success
+  console.log(response);
+  var optionalemailverification = '';
+            var disableemailverification = '';
+            if (typeof LRObject.options.optionalEmailVerification != 'undefined') {
+                optionalemailverification = LRObject.options.optionalEmailVerification;
+            }
+            if (typeof LRObject.options.disabledEmailVerification != 'undefined') {
+                disableemailverification = LRObject.options.disabledEmailVerification;
+            }                
+            if (response.IsPosted && response.Data == null) {
+                if ((typeof (optionalemailverification) == 'undefined' || optionalemailverification !== true) && (typeof (disableemailverification) == 'undefined' || disableemailverification !== true)) {
+                    setMessage("Email for verification has been sent to your provided email, please check email for further instructions");
+                    
+                }
+            }else if (response.access_token != null && response.access_token != "") {
+                 exchangeMultipassToken(response.access_token);
+              
+            } else if(response.IsPosted && typeof response.Data !== 'undefined' && response.Data!==null && typeof response.Data.Sid !== 'undefined')
+            {
+                setMessage("OTP has been sent to your phone number.");
+            } else if(LRObject.options.otpEmailVerification==true && response.Data==null) {
+                setMessage("OTP has been sent to your email address.");           
+            } else {
+                 setMessage("Email for verification has been sent to your provided email, please check email for further instructions");
+            }  
+ 
 };
-registration_options.onError = function(errors) {
-//On Errors
-console.log(errors);
-setErrorMessage(errors[0].Message);
+registration_options.onError = function (errors) {
+  //On Errors
+ console.log(errors);
+  setErrorMessage(errors[0].Message);
+  
 };
-
-
-
 
 var sl_options = {};
-sl_options.onSuccess = function(response) {
+sl_options.onSuccess = function (response) {
   //On Success
   //Here you get the access token
   console.log(response);
   if (response.IsPosted) {
-    document.getElementsByName("loginradius-socialRegistration")[0].reset();
+    //document.getElementsByName("loginradius-socialRegistration")[0].reset();
     setMessage("Email for verification has been sent to your provided email, please check email for further instructions");
     jQuery('#interfacecontainerdiv').hide();
   } else {
-    exchangeMultipassToken(response.access_token, function (data) {
-      window.location = data.url;
-    });
-  }  };
-sl_options.onError = function(errors) {
+    exchangeMultipassToken(response.access_token);    
+  }
+};
+sl_options.onError = function (errors) {
   //On Errors
-  console.log(errors);
-  // lrLogout();
-  // LRObject.init("logout", logout_options);
   if (window.isSSORedirectedToken) {
     LRObject.init("logout", logout_options);
 
   }
-
-
-   else {
+  else {
     setErrorMessage(errors[0].Message);
   }
 };
@@ -384,56 +400,41 @@ sl_options.onError = function(errors) {
 
 
 //Account Linking
-
-
 var la_options = {};
 la_options.container = "interfacecontainerdiv";
 la_options.templateName = 'loginradiuscustom_tmpl_link';
-la_options.onSuccess = function(response) {
-// On Success
-console.log(response);
+la_options.onSuccess = function (response) {
+  // On Success
+  console.log(response);
 };
-la_options.onError = function(errors) {
-// On Errors
-console.log(errors);
+la_options.onError = function (errors) {
+  // On Errors
+  console.log(errors);
 }
 
 
 
-function lrLogout()
-		{
+function lrLogout() {
+  LRObject.init("logout", logout_options);
+  console.log("logout");
+};
 
-				LRObject.init("logout", logout_options);
-        console.log("logout");
+$(document).ready(function () {
 
-
-
-		};
-
-    $( document ).ready(function() {
-
-      if (window.location.pathname == "/logout/account")
-      {
-        console.log("on the page");
-        LRObject.init("logout", logout_options);
-        console.log("logout");
-      }
-    });
+  if (window.location.pathname == "/logout/account") {
+    console.log("on the page");
+    LRObject.init("logout", logout_options);
+    console.log("logout");
+  }
+});
 
 
 
 function visibleLoadingSpinner(isvisible) {
-      if (isvisible) {
-      $("#loading-spinner").show();
-                                }
-      else {
-      $("#loading-spinner").hide();
-                                }};
-
-    LRObject.$hooks.register('beforeFormRender', function () {
-            visibleLoadingSpinner(true);
-        });
-
-    LRObject.$hooks.register('afterFormRender', function () {
-            visibleLoadingSpinner(false);
-        });
+  if (isvisible) {
+    $("#loading-spinner").show();
+  }
+  else {
+    $("#loading-spinner").hide();
+  }
+};
